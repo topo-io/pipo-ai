90121d5bfc01b4bd2f55d976bf138cb56db8624a
Jérémy Mathon <jeremy@capsule.io>
2024-03-23 15:34:06 -0700

    chore(api): Add pipeline API

---
+++ pipo_ai/web/api/pipeline/__init__.py
@@ -0,0 +1,5 @@
+"""Pipeline API."""
+
+from pipo_ai.web.api.pipeline.views import router
+
+__all__ = ["router"]
--- pipo_ai/web/api/router.py
+++ pipo_ai/web/api/router.py
@@ -1,8 +1,11 @@
 from fastapi.routing import APIRouter
 
-from pipo_ai.web.api import docs, echo, monitoring
+from pipo_ai.web.api import docs, echo, monitoring, pipeline
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+api_router.include_router(
+    pipeline.router, prefix="/pipeline", tags=["pipeline"]
+)
