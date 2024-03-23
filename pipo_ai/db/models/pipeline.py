c7a3e1598f6a2e97341c08283f059ff757618045
Jérémy Mathon <jeremy@capsule.io>
2024-03-23 14:47:00 -0700

    db(pipeline): add pipeline model

--- pipo_ai/db/models/pipeline.py
+++ pipo_ai/db/models/pipeline.py
@@ -1,17 +1,11 @@
+from sqlalchemy import Column, String, Text, Uuid
+
 from pipo_ai.db.base import Base
 
 
 class Pipeline(Base):
     __tablename__ = "pipeline"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
-    user_id = Column(Integer, ForeignKey("user.id"))
-    user = relationship("User", back_populates="pipelines")
-    tasks = relationship(
-        "Task", back_populates="pipeline", cascade="all, delete-orphan"
-    )
-    # ...
+    id = Column(Uuid, primary_key=True)
+    slug = Column(String, nullable=False)
+    code = Column(Text, nullable=False)
