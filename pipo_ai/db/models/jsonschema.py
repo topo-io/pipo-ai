765396a6699e334247130af381f379f89179a037
Jérémy Mathon <jeremy@capsule.io>
2024-03-23 19:03:25 -0700

    chore(db): add JSONSchema model

---
+++ pipo_ai/db/models/jsonschema.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from typing import TYPE_CHECKING
+
+from sqlalchemy import JSON, Column, Uuid
+from sqlalchemy import Enum as SQLAlchemyEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
+
+from pipo_ai.db.base import Base
+
+if TYPE_CHECKING:
+    from pipo_ai.db.models.pipeline import Pipeline
+
+
+class JsonSchemaTypeEnum(str, Enum):
+    input = "input"
+    output = "output"
+
+
+class JSONSchema(Base):
+    __tablename__ = "jsonschema"
+
+    id = Column(Uuid, primary_key=True, default=func.uuid_generate_v4())
+    value = Column(JSON, nullable=False)
+    type = Column(SQLAlchemyEnum(JsonSchemaTypeEnum), nullable=False)
+    # Relationships
+    pipeline_id: Mapped[Uuid] = mapped_column(Uuid)
+    pipeline: Mapped["Pipeline"] = relationship(
+        "Pipeline", back_populates="jsonschemas"
+    )
--- pipo_ai/db/models/pipeline.py
+++ pipo_ai/db/models/pipeline.py
@@ -1,7 +1,13 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Column, String, Text, Uuid
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.sql import func
 
 from pipo_ai.db.base import Base
+
+if TYPE_CHECKING:
+    from pipo_ai.db.models.jsonschema import JSONSchema
 
 
 class Pipeline(Base):
@@ -10,3 +16,6 @@ class Pipeline(Base):
     id = Column(Uuid, primary_key=True, default=func.uuid_generate_v4())
     slug = Column(String, nullable=False, unique=True)
     code = Column(Text, nullable=False)
+    jsonschemas: Mapped[list["JSONSchema"]] = relationship(
+        "JSONSchema", back_populates="pipeline"
+    )
